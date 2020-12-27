a96fef3290c6a860e30ff5cbe42e9c037d727c50
fullred macbook 07-2020 <fullredmacbook07-2020@fullreds-MacBook-Pro.local>
2020-12-27 21:49:41 +0100

    clean hmtl, add migrations to ignore and remove email

--- ebdjango/users/forms.py
+++ ebdjango/users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    #email = forms.EmailField()
 
     class Meta:
         model = User
